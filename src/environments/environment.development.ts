5420e05fccc05744edb16216fa3bb4ef0e75710d
Paveljolak <paveljolak123@gmail.com>
2024-08-02 08:32:50 +0200

    Frontend for azure

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  // apiBase: 'https://healthyendpoints.azurewebsites.net/',
-  // apiBaseUrl: 'https://healthyendpoints.azurewebsites.net/urls',
+  apiBase: 'https://healthyendpoints.azurewebsites.net/',
+  apiBaseUrl: 'https://healthyendpoints.azurewebsites.net/urls',
 
-  apiBase: 'http://localhost:8080',
-  apiBaseUrl: 'http://localhost:8080/urls',
+  //apiBase: 'http://localhost:8080',
+  //apiBaseUrl: 'http://localhost:8080/urls',
 };
